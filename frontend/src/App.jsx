c4fb4b94d684f08d9f64d33fb2ece05e1eddd168
Shubham Kumar Behera <shubham.behera2509@gmail.com>
2026-01-27 21:56:28 +0530

    Added profile page and edit page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './app.css'
+import './index.css'
 
 const App = () => {
   return (
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
+import Layout from './Layout.jsx'
+import HomePage from './pages/HomePage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import EditPage from './pages/EditPage.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<HomePage />} />
+      <Route path='profile/:id/' element={<ProfilePage />} />
+      <Route path='profile/:id/edit' element={<EditPage/>} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
