5578ec00f8e95fb05d5256004f878630c83c76a7
Satyam Bharti <satyambhartijnv0@gmail.com>
2026-01-31 16:11:50 +0530

    separation of files

---
+++ backend/models/postSchema.js
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+const postSchema = new Schema({
+    title : String,
+    content : String,
+    tags : [String],
+    authorRef : {
+        type : Schema.Types.ObjectId,
+        ref : "User",
+    },
+    votes : {
+        type : Number,
+        default : 0,
+    },
+    imageURL : String
+},{
+    timestamps : true
+})
+const Post = model("Post", postSchema);
+
+export default Post;
